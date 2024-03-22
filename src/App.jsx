2082b9421ccd8ba1dfb0157425a816db12b97daf
Sameer <sameersattar1111@gmail.com>
2024-03-22 23:34:53 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 import HomePage from "./Pages/HomePage";
+import JobPage from "./Pages/JobPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage/>} />
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo (3).png";
 
 const Navbar = () => {
   return (
     <nav class="bg-indigo-700 border-b border-indigo-500">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="flex h-20 items-center justify-between">
-        <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-          {/* <!-- Logo --> */}
-          <a
-            class="flex flex-shrink-0 items-center mr-4"
-            href="/index.html"
-          >
-            <img
-              class="h-10 w-auto"
-              src="images/logo.png"
-              alt="React Jobs"
-            />
-            <span class="hidden md:block text-white text-2xl font-bold ml-2">
-              React Jobs
-            </span>
-          </a>
-          <div class="md:ml-auto">
-            <div class="flex space-x-2">
-              <a
-                href="/index.html"
-                class="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-              >
-                Home
-              </a>
-              <a
-                href="/jobs.html"
-                class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-              >
-                Jobs
-              </a>
-              <a
-                href="/add-job.html"
-                class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-              >
-                Add Job
-              </a>
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="flex h-20 items-center justify-between">
+          <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            {/* <!-- Logo --> */}
+            <Link
+              class="flex flex-shrink-0 items-center mr-4"
+              to="/"
+            >
+              <img class="h-10 w-auto" src={logo} alt="React Jobs" />
+              <span class="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
+            </Link>
+            <div class="md:ml-auto">
+              <div class="flex space-x-2">
+                <Link
+                  to="/"
+                  class="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/jobs"
+                  class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Jobs
+                </Link>
+                <Link
+                  to="/add-job"
+                  class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Add Job
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
