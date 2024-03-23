90aa5d359fc710b78a2519344415ea8042e6fb49
Sameer <sameersattar1111@gmail.com>
2024-03-23 23:54:29 +0500

    api calling

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import jobs from "../Jobs.json";
+import React, { useEffect, useState } from "react";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        console.log(res);
+        const data = await res.json();
+        console.log(data);
+        setJobs(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+      fetchJobs();
+    };
+  }, []);
+
   return (
-    <section class="bg-blue-50 px-4 py-10">
-      <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        {isHome ? "Recent Jobs" :   "Browse Jobs" }
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => {
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => {
             return <JobListing job={job} key={job.id} />;
           })}
         </div>
