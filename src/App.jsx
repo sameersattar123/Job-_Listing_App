63393058c04fd082e332605a75e4e3ae546b1cee
Sameer <sameersattar1111@gmail.com>
2024-03-24 21:10:27 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout";
-import Hero from "./components/Hero";
 import HomePage from "./Pages/HomePage";
 import JobPage from "./Pages/JobPage";
 import NotFoundPage from "./Pages/NotFoundPage";
+import JobsPage from "./Pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobPage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage/>} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -3,9 +3,9 @@ import JobListings from '../components/JobListings'
 
 const JobPage = () => {
   return (
-    <section className='bg-blue-50 px-4 py-6'>
-      <JobListings/>
-    </section>
+<div className="">
+  ddd
+</div>
   )
 }
 
